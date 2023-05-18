a49ecf6cdc2241920f92805cfb26f9f4abaad192
Alexandre Piel <alpi10533@eleve.isep.fr>
2023-05-18 22:30:16 +0200

    encore mieux !

---
+++ src/main/java/com/isep/hometrade/dao/BookingRepository.java
@@ -0,0 +1,36 @@
+package com.isep.hometrade.dao;
+
+import com.isep.hometrade.business.BookingEntity;
+import com.isep.hometrade.business.HousingEntity;
+import com.isep.hometrade.business.UserEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<BookingEntity, Long> {
+
+    @Query("select u from BookingEntity u where u.housingEntity = :x and u.status = 'Pending'")
+    List<BookingEntity> findAllPendingByHousing(@Param("x") HousingEntity housingEntity);
+
+    @Query("select u from BookingEntity u where u.housingEntity = :x and u.status = 'Accepted'")
+    List<BookingEntity> findAllAcceptedByHousing(@Param("x") HousingEntity housingEntity);
+
+    @Query("select u from BookingEntity u where u.housingEntity = :x and u.status = 'Declined'")
+    List<BookingEntity> findAllDeclinedByHousing(@Param("x") HousingEntity housingEntity);
+
+    @Query("select u from BookingEntity u where u.userEntity = :x and u.status = 'Pending'")
+    List<BookingEntity> findAllPendingByUser(@Param("x") UserEntity userEntity);
+
+    @Query("select u from BookingEntity u where u.userEntity = :x and u.status = 'Accepted'")
+    List<BookingEntity> findAllAcceptedByUser(@Param("x") UserEntity userEntity);
+
+    @Query("select u from BookingEntity u where u.userEntity = :x and u.status = 'Declined'")
+    List<BookingEntity> findAllDeclinedByUser(@Param("x") UserEntity userEntity);
+
+    @Query("select u from BookingEntity u where u.userEntity = :x")
+    List<BookingEntity> findByUser(@Param("x") UserEntity userEntity);
+
+}
